fece9e503e3ef7633ed8ca015ccc8a85a31ef769
hcy2206 <97283537+hcy2206@users.noreply.github.com>
2023-07-01 23:38:32 +0800

    Use venv to replace conda environment.

--- MAA-Notification.bat
+++ MAA-Notification.bat
@@ -1,2 +1,9 @@
-call conda activate MAA
-python "D:\Green Tools\MAA-Arknights\Persional_Notify\main.py"
+if not exist venv (
+    python -m venv venv
+    call venv\Scripts\activate.bat
+    pip install -r requirements.txt
+)
+
+call venv\Scripts\activate.bat
+
+python main.py
